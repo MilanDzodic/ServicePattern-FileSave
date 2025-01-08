c942d3bae3e31980a98da9b3c40b25e02b74f9b2
Milan Dzodic <milan.dzodic@hotmail.com>
2025-01-08 15:12:33 +0100

    Klart

--- Testing/Helpers/SecurePasswordGenerator_Tests.cs
+++ Testing/Helpers/SecurePasswordGenerator_Tests.cs
@@ -5,14 +5,27 @@ namespace Testing.Helpers;
 public class SecurePasswordGenerator_Tests
 {
   [Fact]
-
-  public void GenerateSecurePassword_ShouldReturnPasswordofTypeString()
+  public void Generate_ShouldReturnValidHash()
   {
-    // act
-    string id = SecurePasswordGenerator.Generate();
+    // Arrange
+    string password = "SecurePassword123!";
+    // Act
+    string hash = SecurePasswordGenerator.Generate(password);
+    // Assert
+    Assert.NotNull(hash);
+    Assert.NotEmpty(hash);
+  }
 
-    // assert
-    Assert.False(string.IsNullOrEmpty(id));
-    Assert.True(Guid.TryParse(id, out _));
+  [Fact]
+  public void Generate_ShouldReturnDifferentHashesForDifferentPasswords()
+  {
+    // Arrange
+    string password1 = "Password1!";
+    string password2 = "Password2!";
+    // Act
+    string hash1 = SecurePasswordGenerator.Generate(password1);
+    string hash2 = SecurePasswordGenerator.Generate(password2);
+    // Assert
+    Assert.NotEqual(hash1, hash2);
   }
 }
